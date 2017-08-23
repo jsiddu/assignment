b0df6a873cf4f387742974acb5aba2d9da202a4d
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-23 16:11:27 +0530

    Create six.cpp

---
+++ six.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio>
+int main()
+{
+ float r;
+ cout<<"enter the radius";
+ cin>>r;
+ cout<<"the diameter is"<<r*2;
+ cout<<"the circumference is"<<2*3.14*r;
+ cout<<"the area is"<<3.14*r*r;
+ getch();
+} 
