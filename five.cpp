b7804d1378a77735521642830420ac8526e4c3aa
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-23 16:02:12 +0530

    Create five.cpp

---
+++ five.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+ int l,b;
+ cout<<"enter the length and breadth of rectangle";
+ cin>>l,b;
+ cout<<"the perimeter is "<<l+b;
+ getch();
+}
+ 
+ 
